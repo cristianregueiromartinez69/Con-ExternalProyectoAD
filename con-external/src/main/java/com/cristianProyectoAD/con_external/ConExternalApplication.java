33de863c526f066a2cb336b63c19b4e8566b0220
cristian <cregueiromartinez@danielcastelao.org>
2024-11-06 18:07:01 +0100

    creando proyecto dividido, estructura registro libros

---
+++ con-external/src/main/java/com/cristianProyectoAD/con_external/ConExternalApplication.java
@@ -0,0 +1,13 @@
+package com.cristianProyectoAD.con_external;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConExternalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConExternalApplication.class, args);
+	}
+
+}
---
+++ con-external/src/test/java/com/cristianProyectoAD/con_external/ConExternalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristianProyectoAD.con_external;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConExternalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
