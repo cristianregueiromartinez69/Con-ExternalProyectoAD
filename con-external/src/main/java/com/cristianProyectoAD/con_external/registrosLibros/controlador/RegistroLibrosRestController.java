4ade578adf30bfc3f22e811064e9165ff478b880
cristian <cregueiromartinez@danielcastelao.org>
2024-11-10 16:10:09 +0100

    documentacion rest controller registro libros

--- con-external/src/main/java/com/cristianProyectoAD/con_external/registrosLibros/controlador/RegistroLibrosRestController.java
+++ con-external/src/main/java/com/cristianProyectoAD/con_external/registrosLibros/controlador/RegistroLibrosRestController.java
@@ -9,16 +9,31 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Controlador para enviar la información de los libros al prd-rex
+ * @author cristian
+ * @version 1.0
+ */
 @RestController
 @RequestMapping("/bookhub/libros")
 public class RegistroLibrosRestController {
 
+    //atributo del servicio
     private final LibroService libroService;
 
+    /**
+     * Constructor de la clase
+     * @param libroService el servicio de libros
+     */
     public RegistroLibrosRestController(LibroService libroService) {
         this.libroService = libroService;
     }
 
+    /**
+     * Metodo para mandar los libros a otro microservicio
+     * @param libroDto el objeto libros
+     * @return si se guardó o no se guardó
+     */
     @PostMapping("/registro")
     public ResponseEntity<String> registroControllerLibros(@RequestBody LibroDto libroDto) {
         return libroService.registrarLibro(libroDto);
