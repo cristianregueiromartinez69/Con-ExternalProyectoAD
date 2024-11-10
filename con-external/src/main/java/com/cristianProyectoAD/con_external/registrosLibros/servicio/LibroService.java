bc2700264c72cdc1b97f1003ebfcbc118b7d9ece
cristian <cregueiromartinez@danielcastelao.org>
2024-11-10 16:08:38 +0100

    documentacion servicios registro libros

--- con-external/src/main/java/com/cristianProyectoAD/con_external/registrosLibros/servicio/LibroService.java
+++ con-external/src/main/java/com/cristianProyectoAD/con_external/registrosLibros/servicio/LibroService.java
@@ -6,16 +6,31 @@ import com.cristianProyectoAD.con_external.registrosLibros.servicio_comunicacion
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+/**
+ * Clase servicio de registro de libros
+ * @author cristian
+ * @version 1.0
+ */
 @Service
 public class LibroService {
 
 
+    //atributo con el servicio a enviar los datos
     private PrdRexClient prdRexClient;
 
+    /**
+     * Constructor de la clase
+     * @param prdRexClient el cliente al que le vamos a enviar los datos
+     */
     public LibroService(PrdRexClient prdRexClient) {
         this.prdRexClient = prdRexClient;
     }
 
+    /**
+     * Metodo para registrar un libro y mandarlo a otro microservicio
+     * @param libro el objeto libro
+     * @return el registro
+     */
     public ResponseEntity<String> registrarLibro(LibroDto libro) {
         return prdRexClient.registrarLibro(libro);
     }
