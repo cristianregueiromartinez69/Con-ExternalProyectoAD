e93e60e4443aef4c2719c7805679271649e4e016
Cristian <cregueiromartinez@danielcastelao.org>
2024-11-12 13:02:55 +0100

    metodo en cliente para consultar libro por isbn

--- con-external/src/main/java/com/cristianProyectoAD/con_external/registrosLibros/servicio_comunicacion/PrdRexClient.java
+++ con-external/src/main/java/com/cristianProyectoAD/con_external/registrosLibros/servicio_comunicacion/PrdRexClient.java
@@ -4,6 +4,8 @@ package com.cristianProyectoAD.con_external.registrosLibros.servicio_comunicacio
 import com.cristianProyectoAD.con_external.registrosLibros.dto.LibroDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -21,4 +23,7 @@ public interface PrdRexClient {
      */
     @PostMapping("/prd-rex/libros/registro")
     ResponseEntity<String> registrarLibro(@RequestBody LibroDto libroDto);
+
+    @GetMapping("/prd-rex/libros/consulta/isbn/{isbn}")
+    LibroDto consultarLibro(@PathVariable("isbn") String isbn);
 }
