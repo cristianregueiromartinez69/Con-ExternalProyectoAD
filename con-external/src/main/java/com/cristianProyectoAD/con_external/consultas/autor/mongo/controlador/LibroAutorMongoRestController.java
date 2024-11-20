11444fc1df90acc2db6c120091bc41b20e3a5005
cristian <cregueiromartinez@danielcastelao.org>
2024-11-20 18:32:51 +0100

    rest controller consulta libros por autor en mongo

--- con-external/src/main/java/com/cristianProyectoAD/con_external/consultas/autor/mongo/controlador/LibroAutorMongoRestController.java
+++ con-external/src/main/java/com/cristianProyectoAD/con_external/consultas/autor/mongo/controlador/LibroAutorMongoRestController.java
@@ -1,4 +1,34 @@
 package com.cristianProyectoAD.con_external.consultas.autor.mongo.controlador;
 
+import com.cristianProyectoAD.con_external.consultas.autor.mongo.servicio.LibroAutorServiceMongo;
+import com.cristianProyectoAD.con_external.consultas.autor.postgres.servicio.LibroAutorServicePostgres;
+import com.cristianProyectoAD.con_external.registrosLibros.dto.LibroDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/bookhub/libros/consulta/mongoDB")
 public class LibroAutorMongoRestController {
+
+    private final LibroAutorServiceMongo libroAutorServiceMongo;
+
+    public LibroAutorMongoRestController(LibroAutorServiceMongo libroAutorServiceMongo) {
+        this.libroAutorServiceMongo = libroAutorServiceMongo;
+    }
+
+    @GetMapping("/autor{autor}")
+    public ResponseEntity<List<LibroDto>> getLibroByAutor(@PathVariable String autor) {
+        ResponseEntity<List<LibroDto>> response = libroAutorServiceMongo.getLibrosMongoByAutor(autor);
+
+        if(response.getStatusCode() == HttpStatus.NOT_FOUND){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(response.getBody(), HttpStatus.OK);
+    }
 }
