4f886ef7471ee77aa65be6174e8a3cf1d5b36794
cristian <cregueiromartinez@danielcastelao.org>
2024-11-20 16:55:06 +0100

    estructura para consulta de libro por autor

---
+++ con-external/src/main/java/com/cristianProyectoAD/con_external/consultas/autor/Prueba.java
@@ -0,0 +1,4 @@
+package com.cristianProyectoAD.con_external.consultas.autor;
+
+public class Prueba {
+}
---
+++ con-external/src/main/java/com/cristianProyectoAD/con_external/consultas/autor/postgres/Prueba.java
@@ -0,0 +1,4 @@
+package com.cristianProyectoAD.con_external.consultas.autor.postgres;
+
+public class Prueba {
+}
