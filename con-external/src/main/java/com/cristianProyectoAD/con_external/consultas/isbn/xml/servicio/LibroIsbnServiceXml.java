0b89c408432af4b3151b2b794af7ed8d11b4e1c8
Cristian <cregueiromartinez@danielcastelao.org>
2024-11-20 10:42:27 +0100

    rest controller busqueda isbn fichero

--- con-external/src/main/java/com/cristianProyectoAD/con_external/consultas/isbn/xml/servicio/LibroIsbnServiceXml.java
+++ con-external/src/main/java/com/cristianProyectoAD/con_external/consultas/isbn/xml/servicio/LibroIsbnServiceXml.java
@@ -3,7 +3,9 @@ package com.cristianProyectoAD.con_external.consultas.isbn.xml.servicio;
 import com.cristianProyectoAD.con_external.registrosLibros.dto.LibroDto;
 import com.cristianProyectoAD.con_external.registrosLibros.servicio_comunicacion.PrdRexClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+@Service
 public class LibroIsbnServiceXml {
 
     private final PrdRexClient prdRexClient;
