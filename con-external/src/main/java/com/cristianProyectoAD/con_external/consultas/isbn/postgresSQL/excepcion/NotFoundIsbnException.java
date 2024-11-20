c585cd69f39acd1c00bbd558e144b8022da0b1c1
cristian <cregueiromartinez@danielcastelao.org>
2024-11-20 17:01:11 +0100

    servicio de consulta de libro por autor en postgres

--- con-external/src/main/java/com/cristianProyectoAD/con_external/consultas/isbn/postgresSQL/excepcion/NotFoundIsbnException.java
+++
@@ -1,7 +0,0 @@
-package com.cristianProyectoAD.con_external.consultas.isbn.postgresSQL.excepcion;
-
-public class NotFoundIsbnException extends RuntimeException {
-    public NotFoundIsbnException(String message) {
-        super(message);
-    }
-}
