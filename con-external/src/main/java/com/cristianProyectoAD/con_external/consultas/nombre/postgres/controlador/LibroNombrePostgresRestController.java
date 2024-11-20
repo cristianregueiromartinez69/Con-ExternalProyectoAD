1e399d9f3e70f55c485626a1a00735ac6eb89a42
cristian <cregueiromartinez@danielcastelao.org>
2024-11-20 19:28:05 +0100

    haciendo restcontroller de consulta libro por nombre

--- con-external/src/main/java/com/cristianProyectoAD/con_external/consultas/nombre/postgres/controlador/LibroNombrePostgresRestController.java
+++ con-external/src/main/java/com/cristianProyectoAD/con_external/consultas/nombre/postgres/controlador/LibroNombrePostgresRestController.java
@@ -1,11 +1,27 @@
 package com.cristianProyectoAD.con_external.consultas.nombre.postgres.controlador;
 
+import com.cristianProyectoAD.con_external.consultas.nombre.postgres.servicio.LibroNombreServicePostgres;
+import com.cristianProyectoAD.con_external.registrosLibros.dto.LibroDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/bookhub/libros/consulta/postgresSQL")
 public class LibroNombrePostgresRestController {
 
+    private final LibroNombreServicePostgres libroNombreServicePostgres;
 
+    public LibroNombrePostgresRestController(LibroNombreServicePostgres libroNombreServicePostgres) {
+        this.libroNombreServicePostgres = libroNombreServicePostgres;
+    }
+
+    @GetMapping("/nombre{nombre}")
+    public ResponseEntity<List<LibroDto>> getLibrosPostgresByNombre(@PathVariable String nombre) {
+        return libroNombreServicePostgres.getLibrosByNombre(nombre);
+    }
 }
