48e93bbf51b7089de075ad03f8c666c4e0b1cedc
cristian <cregueiromartinez@danielcastelao.org>
2024-11-24 14:18:05 +0100

    creando estructura para consultas entre fechas de registro

---
+++ con-external/src/main/java/com/cristianProyectoAD/con_external/consultas/fecharegistro/Prueba.java
@@ -0,0 +1,4 @@
+package com.cristianProyectoAD.con_external.consultas.fecharegistro;
+
+public class Prueba {
+}
---
+++ con-external/src/main/java/com/cristianProyectoAD/con_external/consultas/fecharegistro/mongo/Prueba.java
@@ -0,0 +1,4 @@
+package com.cristianProyectoAD.con_external.consultas.fecharegistro.mongo;
+
+public class Prueba {
+}
