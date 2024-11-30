f3ab1dc7eeea1997f622d6d01c3b99bc91f28382
cristian <cregueiromartinez@danielcastelao.org>
2024-11-30 15:56:45 +0100

    servicio fecha registro mongodb

--- con-external/src/main/java/com/cristianProyectoAD/con_external/consultas/fecharegistro/mongo/servicio/LibroFechaRegistroServicioMongo.java
+++ con-external/src/main/java/com/cristianProyectoAD/con_external/consultas/fecharegistro/mongo/servicio/LibroFechaRegistroServicioMongo.java
@@ -1,7 +1,23 @@
 package com.cristianProyectoAD.con_external.consultas.fecharegistro.mongo.servicio;
 
+import com.cristianProyectoAD.con_external.registrosLibros.dto.LibroDto;
+import com.cristianProyectoAD.con_external.servicio_comunicacion.PrdRexClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class LibroFechaRegistroServicioMongo {
+
+    private final PrdRexClient prdRexClient;
+
+    public LibroFechaRegistroServicioMongo(PrdRexClient prdRexClient) {
+        this.prdRexClient = prdRexClient;
+    }
+
+    public ResponseEntity<List<LibroDto>> getLibrosMongoFechaRegistro(LocalDate fechaInicio, LocalDate fechaFin) {
+        return prdRexClient.consultaLibroFechaRegistroMongo(fechaInicio, fechaFin);
+    }
 }
